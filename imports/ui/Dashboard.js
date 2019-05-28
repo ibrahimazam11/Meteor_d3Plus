d5b62500ce4b075e599a6746e788928bc55b4d05
Ibrahim Azam <ibrahim_azam@live.com>
2019-05-29 03:30:41 +0500

    C

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -7,53 +7,65 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
     // this.config = props.config;
+    this.state = {
+      config: {
+        "data": [
+          {
+            "key": "Afghanistan",
+            "region": "Asia",
+            "subregion": "Southern Asia",
+            "value": 25500100
+          },
+          {
+            "key": "Åland Islands",
+            "region": "Europe",
+            "subregion": "Northern Europe",
+            "value": 2821977
+          },
+          {
+            "key": "Albania",
+            "region": "Europe",
+            "subregion": "Southern Europe",
+            "value": 2821977
+          },
+          {
+            "key": "Algeria",
+            "region": "Africa",
+            "subregion": "Northern Africa",
+            "value": 37900000
+          },
+          {
+            "key": "American Samoa",
+            "region": "Oceania",
+            "subregion": "Polynesia",
+            "value": 55519
+          },
+          {
+            "key": "Andorra",
+            "region": "Europe",
+            "subregion": "Southern Europe",
+            "value": 76246
+          }
+        ],
+        groupBy: ["region", "subregion", "key"],
+        size: "value"
+      },
+      attributes: [],
+      selectedAttributes: []
+    }
+  }
+
+  componentWillMount() {
+    let arr = []
+    Object.entries(this.state.config.data[0]).forEach(([key, value], index) => {
+      arr.push(key)
+    })
+    this.setState({ attributes: arr })
   }
 
   drawMap() {
-    let config = {
-      "data": [
-        {
-          "key": "Afghanistan",
-          "region": "Asia",
-          "subregion": "Southern Asia",
-          "value": 25500100
-        },
-        {
-          "key": "Åland Islands",
-          "region": "Europe",
-          "subregion": "Northern Europe",
-          "value": 2821977
-        },
-        {
-          "key": "Albania",
-          "region": "Europe",
-          "subregion": "Southern Europe",
-          "value": 2821977
-        },
-        {
-          "key": "Algeria",
-          "region": "Africa",
-          "subregion": "Northern Africa",
-          "value": 37900000
-        },
-        {
-          "key": "American Samoa",
-          "region": "Oceania",
-          "subregion": "Polynesia",
-          "value": 55519
-        },
-        {
-          "key": "Andorra",
-          "region": "Europe",
-          "subregion": "Southern Europe",
-          "value": 76246
-        }
-      ],
-      groupBy: ["region", "subregion", "key"],
-      size: "value"
-    };
-    let data = config.data,
-      groupBy = config.groupBy;
+    let data = this.state.config.data,
+      groupBy = this.state.config.groupBy;
     alert()
     // eslint-disable-next-line
     let visualization = d3plus.viz()
@@ -64,7 +76,7 @@ class Dashboard extends Component {
       .data(data)
       .type("tree_map")
       .id(groupBy)
-      .size(config.size)
+      .size(this.state.config.size)
       .resize(true)
       .draw();
     alert("here")
@@ -92,8 +104,16 @@ class Dashboard extends Component {
       <div>
         <PrivateHeader title="Dashboard" />
         <div className="page-content">
-          Dashboard page content.
-      </div>
+          <table>
+            <tbody>
+              <tr><td>{this.state.config.data[0].region}</td></tr>
+              <tr>{this.state.attributes.map((a) => {
+                <label>{a}</label>
+                console.log(a)
+              })}</tr>
+            </tbody>
+          </table>
+        </div>
         <div id="viz" onLoad={this.drawMap()}></div>
       </div >
     );
