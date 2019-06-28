1519d5de35fe47ce0df2634171b7379216ec93c7
Ibrahim <m.ali19871@outlook.com>
2019-06-28 20:12:16 +0500

    C

--- imports/api/treemapData.js
+++ imports/api/treemapData.js
@@ -3,7 +3,7 @@ import { HTTP } from 'meteor/http'    // built in metoer http package to call ex
 export const httpGet = {  /**** Method to get data from http call ****/
   getTreemapData() {
     return new Promise((res, rej) => {    // create promise for calling api using metor/http package
-      HTTP.call("GET", "https://cloud.iexapis.com/stable/stock/xxii/stats?token=pk_bc4c1f923481462fa077c954d1f8b134", {   // get data from api
+      HTTP.call("GET", "https://cors-anywhere.herokuapp.com/https://cloud.iexapis.com/stable/stock/xxii/stats?token=pk_bc4c1f923481462fa077c954d1f8b134", {   // get data from api
         headers: { "Access-Control-Allow-Origin": "*" },    // for testing purpose only
       }, function (error, result) {
         if (error) {
